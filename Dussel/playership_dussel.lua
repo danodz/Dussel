78197594dc7b73d9ce5cf2b1198ce47c9995eb85
Manuel <manuel.bergeron.d@gmail.com>
2017-05-30 22:29:57 -0400

    Test dans playership

--- Dussel/playership_dussel.lua
+++ Dussel/playership_dussel.lua
@@ -1,13 +1,37 @@
-    
+﻿    
 	-- Name: playership dussel
 	-- comprend les vaisseaux joueurs modifiés
 
     -- Create the main ships for the players.
 	
+function init_ships()
+	
     -- Vindh    
    DIVX = PlayerSpaceship():setFaction("Vindh"):setTemplate("Artheurge")
    DIVX:setTypeName("Camarade DIVX"):setCallSign("DIVX")
-
+   DIVX:setHull(10)
+   A = DIVX:getShieldMax(1) 
+   B = DIVX:getShieldMax(2) 
+   DIVX:setShieldsMax(A+10, B+10)
+   DIVX:setBeamWeapon(0,10, 0, 2000, 6.0, 8)
+   --DIVX:addCustomButton("engineering", "BatteryLow","Battery", function() 
+   --end)
+   
+--[[ DIVX:addCustomButton("engineering", "REFINE_DILITHUIM", "refine dilithuim", function()
+		if DIVX.minerai["Dilithuim"] == 0 then
+			DIVX:addToShipLog("Yttrium not available","red")
+		end
+		
+		if DIVX.minerai["Dilithuim"] > 0 then
+			DIVX.minerai["Dilithuim"] = DIVX.minerai["Dilithuim"] - 1
+			DIVX.update_stock = 1
+			DIVX:setEnergyLevel(DIVX:getEnergyLevel()+200)
+		end
+		a = 12
+	end)]]
+   
+   
+   
    Korosheg = PlayerSpaceship():setFaction("Vindh"):setTemplate("Artheurge"):setCallSign("KoroG")
    Korosheg:setTypeName("Korosheg"):setCallSign("KoroG")
 
@@ -31,4 +55,9 @@
    NexusVoid:setTypeName("NexusVoid"):setCallSign("NV")
 
 
- 
+end
+
+
+
+
+-- LFusion_1 
