b51a4fdaf32f66c2bbbf2742ebd5ec6d6cf7a1e6
Erwan <erwan.bourdeau@gmail.com>
2017-11-19 18:25:32 -0500

    Working dogfight over mothership.

--- scenario_D_00_dogfight.lua
+++ scenario_D_00_dogfight.lua
@@ -1,82 +1,78 @@
 --Name: Combat de chasseurs
-
 function init()
     localMothership = mkConquestMothership("Vindh","local",-5000,0);
     baronMothership = mkConquestMothership("Merillon","baron",5000,0);
     motherships = {localMothership, baronMothership};
-    player = {};
+    localPlayers = {};
+    baronPlayers = {};
+
+    for i=1,12,1 do
+        table.insert(localPlayers, mkPlayer("Vindh", -5000, 0));
+        table.insert(baronPlayers, mkPlayer("Merillon", 5000, 0));
+    end
+
     script = Script()
-    addGMFunction("Update Ships", function() script:run("shipUpdate.lua") end);
 end
 
 function update()
     updateMothership(localMothership, baronMothership);
     updateMothership(baronMothership, localMothership);
+    
+    local lx,ly = localMothership:getPosition();
+    for i,ship in pairs(localPlayers) do
+        if not ship:isValid() then
+            localPlayers[i] = mkPlayer("Vindh", lx,ly);
+        end
+    end
 
+    local bx,by = baronMothership:getPosition();
+    for i,ship in pairs(baronPlayers) do
+        if not ship:isValid() then
+            baronPlayers[i] = mkPlayer("Merillon", bx,by);
+        end
+    end
 end
 
 function updateMothership(ship, enemy)
     ship.data.respawnTimer = ship.data.respawnTimer + 1;
 
-    local hornets = countLivingAndDead(ship.data.troops.hornets).living;
-    local atlantis = countLivingAndDead(ship.data.troops.atlantis).living;
-    --if ship.data.respawnTimer >= 1000 then
-    if ship.data.respawnTimer >= 10 then
+    local hornets = countLivingAndDead(ship.data.troops).living;
+    if ship.data.respawnTimer >= 1000 then
         local x,y = ship:getPosition();
         local ex,ey = enemy:getPosition();
 
-        function addHornets(fn)
-            mobs = generateMobs(irandom(1,3), "MU52 Hornet", ship:getFaction(), x, y, 1000, fn);
+        function addHunters(fn)
+            mobs = generateMobs(irandom(1,3), "Chasseur", ship:getFaction(), x, y, 1000, fn);
             for i,mob in pairs(mobs) do
-                table.insert(ship.data.troops.hornets, mob);
-            end
-        end
-
-        function addAtlantis(fn)
-            mobs = generateMobs(1, "Atlantis X23", ship:getFaction(), x, y, 1000, fn);
-            for i,mob in pairs(mobs) do
-                table.insert(ship.data.troops.atlantis, mob);
+                table.insert(ship.data.troops, mob);
             end
         end
 
         if hornets < 6 then
-            addHornets(function(mob) mob:orderDefendTarget(ship) end);
-            ship.data.respawnTimer = 0;
-        else if atlantis < 1 then
-            addAtlantis(function(mob) mob:orderDefendTarget(ship) end);
+            addHunters(function(mob) mob:orderDefendTarget(ship) end);
             ship.data.respawnTimer = 0;
         else if hornets < 12 then
-            addHornets(function(mob) mob:orderDefendTarget(ship) end);
+            addHunters(function(mob) mob:orderFlyTowards(ex,ey) end);
             ship.data.respawnTimer = 0;
-        else if atlantis < 2 then
-            addAtlantis(function(mob) mob:orderDefendTarget(enemy) end);
-            ship.data.respawnTimer = 0;
-        if hornets < 18 then
-            addHornets(function(mob) mob:orderFlyTowards(ex,ey) end);
-            ship.data.respawnTimer = 0;
-        else if atlantis < 3 then
-            addAtlantis(function(mob) mob:orderFlyTowards(ex,ey) end);
-            ship.data.respawnTimer = 0;
-        else if hornets < 24 then
-            addHornets(function(mob) mob:orderFlyTowards(ex,ey) end);
-            ship.data.respawnTimer = 0;
-        else if atlantis < 4 then
-            addAtlantis(function(mob) mob:orderFlyTowards(ex,ey) end);
+        else if hornets < 18 then
+            addHunters(function(mob) mob:orderAttack(enemy) end);
             ship.data.respawnTimer = 0;
         else
             ship.data.respawnTimer = 0;
-        end end end end end end end end
+        end end end
     end
 end
 
 function mkConquestMothership(faction, callSign, x, y)
     local mothership = PlayerSpaceship():setTemplate("Mothership"):setFaction(faction):setCallSign(callSign):setPosition(x, y);
-    mothership.data = { troops = { hornets = {}
-                                 , atlantis = {}
-                                 }
+    mothership.data = { troops = {}
                       , respawnTimer = 750
                       }
     return mothership;
+end
+
+function mkPlayer(faction, x, y)
+    return PlayerSpaceship():setTemplate("Chasseur"):setFaction(faction):setCallSign(srandom(irandom(2,3)) .. irandom(10,999)):setPosition(x, y)
 end
 
 function countLivingAndDead(...)
@@ -110,7 +106,8 @@ function generateMobs(nb, template, faction, centerX, centerY, radius, fn)
     local mobs = {};
     
     for i=1, nb, 1 do
-        local mob = CpuShip():setFaction(faction):setTemplate(template):setCallSign(srandom(irandom(2,3)) .. irandom(10,999)):setPosition(irandom(centerX - radius, centerX + radius), irandom(centerY - radius, centerY + radius))
+        local mob = CpuShip():setFaction(faction):setTemplate(template):setCallSign(srandom(irandom(2,3)) .. irandom(10,999)):setPosition(irandom(math.floor(centerX - radius), math.floor(centerX + radius)), irandom(math.floor(centerY - radius), math.floor(centerY + radius)
+))
         fn(mob);
         table.insert(mobs, mob);
     end
