7e3a2af68318c3b04a9486b24fe4c261d0d26577
Manuel <manuel.bergeron.d@gmail.com>
2017-11-25 03:04:22 -0500

    Ajustement des scénarios basic et embuscade pour fitter avec nos factions - reste à ajouter les textes d'intro - basic : enlever une-des stations ou rendre aléatoire le nombre de stations à protéger.

---
+++ scenario_D_00_embuscade.lua
@@ -0,0 +1,56 @@
+-- Name: Embuscade
+-- Description: You are surrounded by astroids, enemies and mines.
+-- Type: Basic
+
+function setCirclePos(obj, angle, distance)
+	obj:setPosition(math.sin(angle / 180 * math.pi) * distance, -math.cos(angle / 180 * math.pi) * distance)
+end
+
+function init()
+    SpaceStation():setTemplate("Small Station"):setFaction("Dussel"):setPosition(0, -500):setRotation(random(0, 360))
+    
+    for n=1,5 do
+        ship = CpuShip():setFaction("Loyalistes"):setTemplate("Phobos T3"):orderRoaming()
+		setCirclePos(ship, random(0, 360), random(7000, 10000))
+	end
+    for n=1,2 do
+        ship = CpuShip():setFaction("Loyalistes"):setTemplate("Piranha F12"):orderRoaming()
+		setCirclePos(ship, random(0, 360), random(7000, 10000))
+	end
+	
+	a = random(0, 360)
+	d = 9000
+	ship = CpuShip():setFaction("Loyalistes"):setTemplate("Atlantis X23"):setRotation(a + 180):orderRoaming()
+	setCirclePos(ship, a, d)
+
+    wingman = CpuShip():setFaction("Loyalistes"):setTemplate("MT52 Hornet"):setRotation(a + 180)
+	setCirclePos(wingman, a - 5, d + 100)
+	wingman:orderFlyFormation(ship, 500, 100)
+
+    wingman = CpuShip():setFaction("Loyalistes"):setTemplate("MT52 Hornet"):setRotation(a + 180)
+	setCirclePos(wingman, a + 5, d + 100)
+	wingman:orderFlyFormation(ship, -500, 100)
+
+    wingman = CpuShip():setFaction("Loyalistes"):setTemplate("MT52 Hornet"):setRotation(a + 180)
+	setCirclePos(wingman, a + random(-5, 5), d - 500)
+	wingman:orderFlyFormation(ship, 0, 600)
+	
+	
+    for n=1,10 do
+        setCirclePos(Mine(), random(0, 360), random(10000, 20000))
+    end
+    
+    for n=1, 300 do
+        setCirclePos(Asteroid(), random(0, 360), random(10000, 20000))
+    end
+
+    Script():run("playership/all_playership.lua");
+
+end
+
+function update(delta)
+	--No victory condition
+end
+
+
+
