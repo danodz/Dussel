50849aedf9f9791e0c365bed2fa3df13c78431f2
Erwan <erwan.bourdeau@gmail.com>
2018-02-06 18:40:59 -0500

    Scenario dussel 2 is better

--- D_01_dussel_02.lua
+++ D_01_dussel_02.lua
@@ -3,8 +3,7 @@ require("utils.lua");
 
 players = {};
 availableItems = { }
-specterStations = {};
-nbVulnerable = 3;
+specterStationPairs = {};
 
 
 
@@ -15,32 +14,57 @@ nbVulnerable = 3;
 function init()
     --[[{{playership/vafJerrold.lua}}]]--
     --[[{{playership/succubiCherubim.lua}}]]--
-    spawnVafJerrold(187213, -88611);
-    spawnSuccubiCherubim(187213, -88611);
+    spawnVafJerrold(0, 0);
+    spawnSuccubiCherubim(0, 0);
 
-    mkSpecterStation(-20000, 0, "DS7")
-    mkSpecterStation(20000, 0, "DS8")
+    dusselStation =  SpaceStation():setTemplate("Large Station"):setFaction("Dussel"):setCallSign("Dussel4"):setPosition(0, 0);
+
+    mkSpecterStationPair(-20000, 0, "DS7", 20000, 0, "DS8")
+    mkSpecterStationPair(0, -20000, "DS7", 0, 20000, "DS8")
 end
 
 function update()
     local allHacked = true;
-    for i,station in pairs(specterStations) do
-        if station.hackedCounter <= 0 then
+    for i,pair in pairs(specterStationPairs) do
+        if pair[1]:getFaction() == "Spectre" then
             allHacked = false;
-            station.spawnWave = station.spawnWave + 1;
-            if station.spawnWave >= 2 * 60 * 60 then
-                local x,y = station:getPosition();
-                mergeTables( station.specters, generateMobs(3, "Atlantis X23", "Spectre", x, y, 1000, function(mob) mob:orderDefendTarget(station) end));
-                station.spawnWave = 0;
+            local bothHacked = true;
+            for i,station in pairs(pair) do
+                station.spawnWave = station.spawnWave + 1;
+                if station.hackedCounter <= 0 then
+                    station:setFaction("Spectre");
+                    bothHacked = false;
+                    if station.spawnWave >= 2 * 60 * 60 then
+                        local x,y = station:getPosition();
+                        mergeTables( station.specters, generateMobs(1, "Atlantis X23", "Spectre", x, y, 1000, function(mob) mob:orderDefendTarget(station) end));
+                        generateMobs(1, "Atlantis X23", "Spectre", x, y, 1000, function(mob) mob:orderAttack(dusselStation) end);
+                        station.spawnWave = 0;
+                    end
+                else
+                    station.hackedCounter = station.hackedCounter - 1; 
+                    print(station:getCallSign());
+                end
             end
-        else
-            station.hackedCounter = station.hackedCounter - 1; 
+    
+            if bothHacked then
+                for i,station in pairs(pair) do
+                    station:setFaction("Dussel");
+                end
+            end
         end
     end
-
     if allHacked then
         victory("Arianne");
     end
+end
+
+function mkSpecterStationPair(x,y, callSign, x2,y2, callSign2)
+    station1 = mkSpecterStation(x,y,callSign);
+    station1:setCommsFunction(specterStationCommsFn(callSign2));
+
+    station2 = mkSpecterStation(x2,y2,callSign2);
+    station2:setCommsFunction(specterStationCommsFn(callSign));
+    table.insert(specterStationPairs, { station1, station2 });
 end
 
 function mkSpecterStation(x,y, callSign)
@@ -48,14 +72,17 @@ function mkSpecterStation(x,y, callSign)
     station.spawnWave = 1000000000;
     station.specters = {};
     station.hackedCounter = 0;
-    station:setCommsFunction(function()
+    return station
+end
+
+function specterStationCommsFn(pairedCallSign)
+    return function()
         setCommsMessage("Bzzzzt");
-        if allDead(comms_target.specters) then
+        if allDead(comms_target.specters) and comms_target.hackedCounter <= 0 then
             addCommsReply("Hacker la station", function()
-                setCommsMessage("Sauvez les autres station aussi");
-                station.hackedCounter = 1 * 60 * 60;
+                setCommsMessage("Bzzzt"..pairedCallSign.."bzzzzt");
+                comms_target.hackedCounter = 2 * 60 * 66;
             end);
         end
-    end);
-    table.insert(specterStations, station);
+    end
 end
